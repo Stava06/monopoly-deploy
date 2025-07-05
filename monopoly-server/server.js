a2c9b7dbc2657b1f5891db446b1b18bd3d2810f3
Stava06 <stav.avraham@e.braude.co.il>
2025-07-06 01:31:09 +0300

    fix server

--- monopoly-server/server.js
+++ monopoly-server/server.js
@@ -28,7 +28,7 @@ function savePlayers() {
 
 // Serve frontend if built
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 io.on("connection", (socket) => {
