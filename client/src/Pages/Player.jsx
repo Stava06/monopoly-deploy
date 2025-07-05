96c3c60dd08360094ab7dab12793827f1d577184
Stava06 <stav.avraham@e.braude.co.il>
2025-07-06 01:10:21 +0300

    again

--- client/src/Pages/Player.jsx
+++ client/src/Pages/Player.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://monopolybank-av36.onrender.com");
+const socket = io(import.meta.env.VITE_SERVER_URL);
 
 const Player = () => {
   const [nickname, setNickname] = useState("");
