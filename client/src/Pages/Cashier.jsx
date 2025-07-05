fb4cd64fe376be645f755352b4542d48e3f4077e
Stava06 <stav.avraham@e.braude.co.il>
2025-07-06 01:06:53 +0300

    fixed server adress

--- client/src/Pages/Cashier.jsx
+++ client/src/Pages/Cashier.jsx
@@ -4,7 +4,7 @@ import useSound from "use-sound";
 import CashSound from "../assets/sounds/cash.mp3";
 
 // Connect to the backend server
-const socket = io("https://monopolybank-av36.onrender.com");
+const socket = io(import.meta.env.VITE_SERVER_URL);
 
 
 const Cashier = () => {
